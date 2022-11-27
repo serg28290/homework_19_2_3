8fc0af83b034e7718426e80ca57eafc82b9ee53e
serg28290 <116976679+serg28290@users.noreply.github.com>
2022-11-27 16:49:35 +0500

    Update calculator.py

--- app/calculator.py
+++ app/calculator.py
@@ -1,1 +1,17 @@
+class Calculator:
+  
+    @staticmethod
+    def multiply(x, y):
+        return x * y
 
+    @staticmethod
+    def division(x, y):
+        return x / y
+
+    @staticmethod
+    def subtraction(x, y):
+        return x - y
+
+    @staticmethod
+    def adding(x, y):
+        return x + y
