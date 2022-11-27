c77606a54bc553634e4b409052fcc5f67d7852cd
serg28290 <116976679+serg28290@users.noreply.github.com>
2022-11-27 16:51:46 +0500

    Update first_test.py

--- tests/first_test.py
+++ tests/first_test.py
@@ -1,1 +1,18 @@
+from scripts.skillfactory.homework_19_2_3.app.calculator import Calculator
 
+
+class TestCalc(Calculator):
+    def setup(self):
+        self.calc = Calculator
+
+    def test_multiply_calculate_correctly(self):
+        assert self.calc.multiply(2, 2) == 4
+
+    def test_division_calculate_correctly(self):
+        assert self.calc.division(6, 3) == 2
+
+    def test_multiply_subtraction_correctly(self):
+        assert self.calc.subtraction(5, 4) == 1
+
+    def test_adding_calculate_correctly(self):
+        assert self.calc.adding(8, 2) == 10
